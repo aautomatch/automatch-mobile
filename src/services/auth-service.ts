5cfa5e3749c384a559f03a14c578f9c5b6986ce8
Diego <aautomatch@gmail.com>
2026-01-09 16:08:14 -0300

    Set env

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -2,16 +2,14 @@ import { LoginRequest } from "../app/types/login-request";
 import { TokenResponse } from "../app/types/toke-response";
 import { User } from "../app/types/user";
 import { api } from "./api";
-//TODO lembrar de gravar o accesstoken no localStorage(no response do loggin):
-//localStorage.setItem("accessToken", response.data.accessToken);
-const AUTH_URL = "localhost:8080/auth";
+
 
 export const AuthService = {
   register(user: User) {
-    return api.post<User>(`${AUTH_URL}/register`, user);
+    return api.post<User>(`${process.env.API_URL}/register`, user);
   },
 
   login(data: LoginRequest) {
-    return api.post<TokenResponse>(`${AUTH_URL}/login`, data);
+    return api.post<TokenResponse>(`${process.env.API_URL}/login`, data);
   }
 };
