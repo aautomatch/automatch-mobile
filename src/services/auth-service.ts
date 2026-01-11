3db33949e8580781173be993a4fff81f8380d1ec
jhonnyvmsantos <mjhonnyvieira@gmail.com>
2026-01-11 13:26:20 -0300

    auth-fix

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -3,15 +3,14 @@ import { TokenResponse } from "../app/types/toke-response";
 import { User } from "../app/types/user";
 import { api } from "./api";
 
-// const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const AuthService = {
   register(user: User) {
-    console.log(user)
-    return api.post<User>(`localhost:8080/auth/register`, user);
+    return api.post<User>(`${API_URL}/auth/register`, user);
   },
 
   login(data: LoginRequest) {
-    return api.post<TokenResponse>(`/auth/login`, data);
+    return api.post<TokenResponse>(`${API_URL}/auth/login`, data);
   }
 };
